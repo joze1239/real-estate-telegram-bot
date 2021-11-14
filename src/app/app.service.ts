d158dabfb04b3709adfa73dad34e7385ed230b35
Joze Fartek <joze.fartek@gmail.com>
2021-11-14 12:15:33 +0100

    Update app service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ApiStatusDto } from './dto/api-status.dto';
 
 @Injectable()
 export class AppService {
     constructor(private configService: ConfigService) {}
 
-    async getStatus(): Promise<any> {
+    async getStatus(): Promise<ApiStatusDto> {
         return {
             environment: this.configService.get<string>('STAGE'),
         };
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '~app/app.module';
 
 async function bootstrap() {
-    console.log(
-        'process.env.TELEGRAM_BOT_TOKEN',
-        process.env.TELEGRAM_BOT_TOKEN,
-    );
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
