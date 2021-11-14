9c791a9f2251b533a44fb8e03925445b4beda894
Joze Fartek <joze.fartek@gmail.com>
2021-11-14 16:05:57 +0100

    Add telegram bot module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
+import { BotModule } from '~modules/bot/bot.module';
 import { CrawlerModule } from '~modules/crawler/crawler.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
         ConfigModule.forRoot(),
         TelegrafModule.forRoot({
             token: process.env.TELEGRAM_BOT_TOKEN,
+            include: [BotModule],
         }),
         CrawlerModule,
     ],
