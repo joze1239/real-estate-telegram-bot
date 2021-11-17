1fe3c063621cf679722e94d0fbdbe625dd1890b3
Joze Fartek <joze.fartek@gmail.com>
2021-11-17 20:07:55 +0100

    Bot module fix

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SubscriptionModule } from '~modules/subscription/subscription.module';
 import { BotService } from './bot.service';
 import { BotUpdate } from './bot.update';
 
 @Module({
+  imports: [SubscriptionModule],
   providers: [BotUpdate, BotService],
 })
 export class BotModule {}
