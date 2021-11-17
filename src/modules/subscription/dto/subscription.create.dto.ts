bc17e1d9cd96cf496ac31d8591ff0feeed01b697
Joze Fartek <joze.fartek@gmail.com>
2021-11-17 20:23:10 +0100

    Create subscription validation

--- src/modules/subscription/dto/subscription.create.dto.ts
+++ src/modules/subscription/dto/subscription.create.dto.ts
@@ -1,12 +1,14 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString, MinLength } from 'class-validator';
 
 export class CreateSubscriptionDto {
   @IsNumber()
   chatId: number;
 
   @IsString()
+  @MinLength(1)
   name: string;
 
   @IsString()
+  @MinLength(1)
   url: string;
 }
