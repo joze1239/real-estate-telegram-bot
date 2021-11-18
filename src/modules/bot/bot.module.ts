0d1b16525f3a66ececeeefa4b7be9638aa9e5263
Joze Fartek <joze.fartek@gmail.com>
2021-11-18 23:05:32 +0100

    List real estates

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CrawlerModule } from '~modules/crawler/crawler.module';
 import { SubscriptionModule } from '~modules/subscription/subscription.module';
 import { BotService } from './bot.service';
 import { BotUpdate } from './bot.update';
 
 @Module({
-  imports: [SubscriptionModule],
+  imports: [SubscriptionModule, CrawlerModule],
   providers: [BotUpdate, BotService],
 })
 export class BotModule {}
