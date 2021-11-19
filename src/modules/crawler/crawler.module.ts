38083f02d728d4bcfec40e6d785b5ad058cc0738
Joze Fartek <joze.fartek@gmail.com>
2021-11-19 23:29:45 +0100

    Crawler update

--- src/modules/crawler/crawler.module.ts
+++ src/modules/crawler/crawler.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CrawlerController } from './crawler.controller';
 import { CrawlerService } from './crawler.service';
 
 @Module({
   providers: [CrawlerService],
   exports: [CrawlerService],
-  controllers: [CrawlerController],
 })
 export class CrawlerModule {}
