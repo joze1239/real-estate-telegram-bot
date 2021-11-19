c700d202ed4bf792f0d065de746292285a9c9619
Joze Fartek <joze.fartek@gmail.com>
2021-11-19 23:32:14 +0100

    Page view module fix

--- src/modules/page-view/page-view.module.ts
+++ src/modules/page-view/page-view.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import PageView from './page-view.entity';
+import { PageViewRepository } from './page-view.repository';
 import { PageViewService } from './page-view.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PageView, PageViewRepository])],
   providers: [PageViewService],
   exports: [PageViewService],
 })
