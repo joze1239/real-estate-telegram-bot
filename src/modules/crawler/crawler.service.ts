8a39434a7fc5d06ef6e1807a4a6ed6e658f5096c
Joze Fartek <joze.fartek@gmail.com>
2022-08-10 21:29:06 +0200

    FIx www.auto.net crawler

--- src/modules/crawler/crawler.service.ts
+++ src/modules/crawler/crawler.service.ts
@@ -5,15 +5,19 @@ import { websites } from './websites';
 
 @Injectable()
 export class CrawlerService {
-  url: string;
-
   public async crawlPage(url: string): Promise<string[]> {
     const website = websites.find((website) => url.includes(website.domain));
     if (!websites) {
       throw new Error('Unknown domain');
     }
 
-    const links = await this.crawlUrlList(url, website.linkSelector);
+    let links = await this.crawlUrlList(url, website.linkSelector);
+
+    // Resolve relative paths
+    links = links.map((link) =>
+      link.startsWith('../') ? link.substring(2) : link,
+    );
+
     const uniqueLinks = [...new Set(links)];
     return uniqueLinks.map((link) => `${website.domain}${link}`);
   }
@@ -22,7 +26,15 @@ export class CrawlerService {
     url: string,
     elementSelector: string,
   ): Promise<string[]> {
-    const res = await axios.get(url, { maxRedirects: 50 });
+    const headers = {
+      'user-agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    };
+    const res = await axios.get(url, {
+      headers,
+    });
+
     const $ = cheerio.load(res.data);
     return $(elementSelector)
       .get()
