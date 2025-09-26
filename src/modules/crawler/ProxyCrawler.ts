a9f6d709e62834d3fded85f9d47d57d8e500f6fb
Joze Fartek <joze.fartek@gmail.com>
2025-09-26 23:57:45 +0200

    fix: proxy crawler

--- src/modules/crawler/ProxyCrawler.ts
+++ src/modules/crawler/ProxyCrawler.ts
@@ -3,18 +3,19 @@ import { Crawler } from './Crawler';
 
 export class ProxyCrawler extends Crawler {
   async getHtml(url: string): Promise<string> {
-    const response = await axios.post('https://api.brightdata.com/request', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.BRIGHT_DATA_API_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
+    const response = await axios.post(
+      'https://api.brightdata.com/request',
+      {
         url,
         zone: process.env.BRIGHT_DATA_ZONE,
         format: 'raw',
-      }),
-    });
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BRIGHT_DATA_API_TOKEN}`,
+        },
+      },
+    );
 
     return response.data;
   }
